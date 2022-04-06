adf71c72b7f5b95da0f30c1cc002969e0aef81eb
Felipe Teixeira <felipeteixeirams@gmail.com>
2022-04-06 15:23:44 -0400

    Aperfeiçoado a animação de escurecer a tela

--- assets/scripts/background.js
+++ assets/scripts/background.js
@@ -2,17 +2,24 @@ const body  = document.getElementById('body');
 const title  = document.getElementById('title');
 const contact = document.getElementById('contact');
 const copyright  = document.getElementById('copyright');
+const linkHost  = document.getElementById('link-host');
+const linkConsult = document.getElementById('link-consult');
+
 
 function changeBgDark() {
-    body.style.backgroundColor = "#888888";
-    title.style.color = "#eeeeee";
-    contact.style.color = "#eeeeee";
-    copyright.style.color = "#eeeeee";
+    body.style.backgroundColor = "var(--grey)";
+    title.style.color = "var(--grey-light)";
+    contact.style.color = "var(--grey-light)";
+    copyright.style.color = "var(--grey-light)";
+    linkHost.style.color = "var(--grey-light)";
+    linkConsult.style.color = "var(--grey-light)";
 }
 
 function changeBgWhite() {
-    body.style.backgroundColor = "#fff";
-    title.style.color = "#333333";
-    contact.style.color = "#333333";
-    copyright.style.color = "#333333";
+    body.style.backgroundColor = "var(--white)";
+    title.style.color = "var(--grey-medium)";
+    contact.style.color = "var(--grey-dark)";
+    copyright.style.color = "var(--grey)";
+    linkHost.style.color = "var(--grey)";
+    linkConsult.style.color = "var(--grey)";
 }
